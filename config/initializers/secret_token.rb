e6c25da4eadec990c8daef9e5c3702283f372863
Dave O'Brien <davecobrien@gmail.com>
2013-02-11 20:48:26 -0600

    Committing for the very first time … as a commitaphobe.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R4rStarter::Application.config.secret_token = '4e121d468743623b53976fea041ee03e8be08288ea504bd9c9d35e5a40969f807e08fe87d1ec5eb3d7c8057a65cd0cb43cf9489e27d16e503f8806b313f17347'
